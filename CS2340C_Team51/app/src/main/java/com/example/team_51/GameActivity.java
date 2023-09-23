c5285b7ffbc9a07b746968fd9d5980b075d7c0f6
dlee3464 <djlee3464@gmail.com>
2023-09-23 19:30:23 -0400

    remove import statement from MainActivity

--- CS2340C_Team51/app/src/main/java/com/example/team_51/GameActivity.java
+++ CS2340C_Team51/app/src/main/java/com/example/team_51/GameActivity.java
@@ -2,20 +2,20 @@ package com.example.team_51;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ViewFlipper;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cs2340c_team51.R;
 
 public class GameActivity extends AppCompatActivity {
-    private ViewFlipper viewFlipper;
+    private Button end;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
 
-        viewFlipper = findViewById(R.id.view_flipper);
+        end = (Button) findViewById(R.id.end);
     }
 
     public void endScreen(View v) {

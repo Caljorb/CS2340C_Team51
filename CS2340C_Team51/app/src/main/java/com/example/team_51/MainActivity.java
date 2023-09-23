ba2ef0ab7fdc5ddae1c2837fff24d05ecd7ca3d3
dlee3464 <djlee3464@gmail.com>
2023-09-23 01:20:04 -0400

    test

--- CS2340C_Team51/app/src/main/java/com/example/team_51/MainActivity.java
+++ CS2340C_Team51/app/src/main/java/com/example/team_51/MainActivity.java
@@ -11,7 +11,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.cs2340c_team51.R;
 
 public class MainActivity extends AppCompatActivity {
-    TextView txt1;
     @Override
     private Button exit;
     protected void onCreate(Bundle savedInstanceState) {

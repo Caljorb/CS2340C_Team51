af25cceffe6c631ea4730ea8ab3c7dc085aa7088
dlee3464 <djlee3464@gmail.com>
2023-09-23 11:54:26 -0400

    trivial

--- CS2340C_Team51/app/src/main/java/com/example/team_51/MainActivity.java
+++ CS2340C_Team51/app/src/main/java/com/example/team_51/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 import android.graphics.Color;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.content.Intent;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,9 +16,8 @@ import com.example.cs2340c_team51.R;
 public class MainActivity extends AppCompatActivity {
     @Override
 
-    private Button exit;
     private Button button;
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
@@ -27,15 +29,6 @@ public class MainActivity extends AppCompatActivity {
         // one thing to note: i think each screen requires its own "activity"
         // https://developer.android.com/guide/components/activities/intro-activities
         // use this w/ example repo to figure out how to make an activity
-
-        exit=findViewById(R.id.Exit);
-        exit.setOnClickListener(new_View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, activity_end_screen.xml);
-                startActivity(intent);
-            }
-        }
 
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {

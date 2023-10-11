bd5134750b53855089ce82d3f1e3da2f44c80185
Caljorb <79600846+Caljorb@users.noreply.github.com>
2023-10-11 16:14:41 -0400

    Completed test case for if leaderboard is a singleton

--- CS2340C_Team51/app/src/test/java/com/example/cs2340c_team51/ExampleUnitTest.java
+++ CS2340C_Team51/app/src/test/java/com/example/cs2340c_team51/ExampleUnitTest.java
@@ -4,6 +4,17 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import android.content.Context;
+
+import com.example.team_51.model.Button;
+import com.example.team_51.model.LeaderboardRow;
+import com.example.team_51.model.Player;
+import com.example.team_51.model.map.Tilemap;
+import com.example.team_51.viewmodels.LeaderboardViewModel;
+import com.example.team_51.viewmodels.SpriteSheet;
+
+import java.util.ArrayList;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -25,17 +36,47 @@ public class ExampleUnitTest {
 
     // Method to check starting health based on difficulty
 
-    // Method to check that map switched upon button press (Caleb)
+    // Method to check that leaderboardViewModel is a singleton (Caleb)
     @Test
-    public void isMapSwitched() {
+    public void isLeaderboardSingleton() {
+        LeaderboardViewModel leaderboardViewModel1 = LeaderboardViewModel.getLeaderboardViewModel();
 
+        int address1 = System.identityHashCode(leaderboardViewModel1);
+        System.out.println("Memory: " + address1);
+
+        LeaderboardViewModel leaderboardViewModel2 = LeaderboardViewModel.getLeaderboardViewModel();
+        int address2 = System.identityHashCode(leaderboardViewModel2);
+        System.out.println("Memory: " + address2);
+
+        assertEquals("Addresses should point to same spot in memory.", address1, address2);
+
+        String beforeSet = leaderboardViewModel1.toString();
+        System.out.println(beforeSet);
+        leaderboardViewModel2.setRows(makeTestRows());
+        String afterSet = leaderboardViewModel1.toString();
+        System.out.println(afterSet);
+
+        assertNotEquals("Changing 2nd reference should also change 1st" +
+                " because they reference same instance", beforeSet, afterSet);
+
+        assertEquals("Changing one should change both", afterSet,
+                leaderboardViewModel2.toString());
     }
 
     // Method to check that selected character picks correct sprite (Caleb)
     @Test
     public void correctSprite() {
-        
+        // idk
     }
 
     // Method to check selecting difficulty updates the text for difficulty
+
+
+    private ArrayList<LeaderboardRow> makeTestRows() {
+        ArrayList<LeaderboardRow> leaderboardRows = new ArrayList<>(5);
+        for (int i = 0; i < 5; i++) {
+            leaderboardRows.add(new LeaderboardRow("Bob" + i, i * 2, "Today"));
+        }
+        return leaderboardRows;
+    }
 }

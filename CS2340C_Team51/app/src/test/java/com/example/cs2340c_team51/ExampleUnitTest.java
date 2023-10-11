c2b245ace2b77c42ed778d642ea50a88e4737f0f
Caljorb <79600846+Caljorb@users.noreply.github.com>
2023-10-11 13:41:38 -0400

    Added layout for tests

--- CS2340C_Team51/app/src/test/java/com/example/cs2340c_team51/ExampleUnitTest.java
+++ CS2340C_Team51/app/src/test/java/com/example/cs2340c_team51/ExampleUnitTest.java
@@ -13,5 +13,29 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    } // example test for reference
+
+    // Method for score not going below 0 here
+
+    // Method for Leaderboard sort scores properly here
+
+    // Method for score being put in leaderboard even when score == 0
+
+    // Method to check name has NO whitespace at all, no null name, or empty name
+
+    // Method to check starting health based on difficulty
+
+    // Method to check that map switched upon button press (Caleb)
+    @Test
+    public void isMapSwitched() {
+
     }
+
+    // Method to check that selected character picks correct sprite (Caleb)
+    @Test
+    public void correctSprite() {
+        
+    }
+
+    // Method to check selecting difficulty updates the text for difficulty
 }

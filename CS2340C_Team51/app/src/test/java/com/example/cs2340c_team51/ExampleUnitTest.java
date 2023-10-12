5aaf9c13124045b5cba396e46a929fcd9f3e9009
Caljorb <79600846+Caljorb@users.noreply.github.com>
2023-10-11 22:30:25 -0400

    Completed checkstyle

--- CS2340C_Team51/app/src/test/java/com/example/cs2340c_team51/ExampleUnitTest.java
+++ CS2340C_Team51/app/src/test/java/com/example/cs2340c_team51/ExampleUnitTest.java
@@ -22,10 +22,6 @@ import java.util.ArrayList;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    } // example test for reference
 
     // Method for score not going below 0 here (Rashmith)
 

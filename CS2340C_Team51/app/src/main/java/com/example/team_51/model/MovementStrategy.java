0f2743dd1878ee2631fd95914cbea5178264e3f1
Caljorb <79600846+Caljorb@users.noreply.github.com>
2023-10-16 19:01:34 -0400

    Updated movement strategy to include only one method for movement, along with comments

--- CS2340C_Team51/app/src/main/java/com/example/team_51/model/MovementStrategy.java
+++ CS2340C_Team51/app/src/main/java/com/example/team_51/model/MovementStrategy.java
@@ -1,8 +1,12 @@
 package com.example.team_51.model;
 
+import android.view.KeyEvent;
+
 public interface MovementStrategy {
-    abstract boolean moveLeft(); // take in keyboard press?
-    abstract boolean moveRight();
-    abstract boolean moveUp();
-    abstract boolean moveDown();
+    abstract boolean moveOnPress(int keyCode, KeyEvent keyEvent); // use update pos method in player
+    // use switch, take in keyCode, use case KeyEvent.KEYCODE_DPAD_XXXX
+    // LEFT, RIGHT, UP, DOWN, add to player X or Y
+    // at end, call check collision
+    // can also call checkOutOfBounds
+    // return true
 }

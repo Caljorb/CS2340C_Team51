f22cbd68005b24d2f6c4538df77eac821e868166
Caljorb <79600846+Caljorb@users.noreply.github.com>
2023-11-12 14:43:18 -0500

    Checked observer pattern, and cleaned up code

--- CS2340C_Team51/app/src/main/java/com/example/team_51/model/enemies/BatFactory.java
+++ CS2340C_Team51/app/src/main/java/com/example/team_51/model/enemies/BatFactory.java
@@ -1,8 +1,5 @@
 package com.example.team_51.model.enemies;
 
-import android.graphics.Canvas;
-
-import com.example.team_51.viewmodels.GameDisplay;
 import com.example.team_51.viewmodels.SpriteSheet;
 
 public class BatFactory extends EnemyFactory {
